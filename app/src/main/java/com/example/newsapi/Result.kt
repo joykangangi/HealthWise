4c2aa482176cab4e5facb9aacffa020507f0ed37
joykangangi <64706463+joykangangi@users.noreply.github.com>
2021-10-12 22:23:41 +0300

    Room setUp

--- app/src/main/java/com/example/newsapi/Result.kt
+++ app/src/main/java/com/example/newsapi/Result.kt
@@ -1,6 +1,15 @@
 package com.example.newsapi
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(
+    tableName = "articles"
+)
 data class Result(
+    @PrimaryKey(autoGenerate = true)
+    val id: Long? = null,//not all articles will retrieved will be saved into the db,
+    // e.g those displayed in the search and common diseases tabs
     val Error: String,
     val Items: Items,
     val Language: String,
